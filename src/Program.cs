1b5308ad9e9d0b0934a75c72b60d0b5d78370589
ALLAL <aallal73@gmail.com>
2025-12-26 14:22:52 +0000

    RegEX validation and extraction implementation

--- src/Program.cs
+++ src/Program.cs
@@ -1,41 +1,38 @@
-﻿namespace ExpReg;
+﻿using System.Text.RegularExpressions;
 
-class Program
+void Afficher(string[] liste, Range range)
 {
-    static void Afficher(string[] liste, Range range)
+    foreach (var c in liste[range])
     {
-        foreach (var c in liste[range])
-        {
-            Console.WriteLine(c);
-        }
-
+        Console.WriteLine(c);
     }
 
-    static void Main(string[] args)
-    {
-        var bic = "ATCICIAB";
-
-        Console.Write($"Le BIC '{bic}' est ");
-        if (bic is not null) // TODO : [A-Z]{6}[A-Z0-9]{2,5}
-        {
-            Console.WriteLine("valide.");
-        }
-        else
-        {
-            Console.WriteLine("non valide.");
-        }
-
-        var twitGeorgeSand = "Tu fuyais la #solitude et la trouvait #partout.";
-
-        var hashtags = Array.Empty<string>(); // TODO : Extraire #\w+
-
-        foreach (var tag in hashtags)
-        {
-            Console.WriteLine(tag);
-        }
-
-        var html = twitGeorgeSand;
-
-        Console.WriteLine(html);
-    }
 }
+
+var bic = "ATCICIAB";
+
+Console.Write($"Le BIC '{bic}' est ");
+if (Regex.IsMatch(bic, @"^[A-Z]{6}[A-Z0-9]{2,5}$")) // TODO : [A-Z]{6}[A-Z0-9]{2,5}
+{
+    Console.WriteLine("valide.");
+}
+else
+{
+    Console.WriteLine("non valide.");
+}
+
+var twitGeorgeSand = "Tu fuyais la #solitude et la trouvait #partout.";
+
+var hashtags = Regex.Matches(twitGeorgeSand, @"#\w+"); // TODO : Extraire #\w+
+
+foreach (var tag in hashtags)
+{
+    Console.WriteLine(tag);
+}
+
+
+var html = Regex.Replace(
+        twitGeorgeSand, @"#\w+",
+        m => $"<a href='?tag={m.Value.Substring(1)}'>{m.Value}</a>"
+);
+Console.WriteLine(html);
